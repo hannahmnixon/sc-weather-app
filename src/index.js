ccf5b7074b458d4df0771993168af07624f36966
hannahmnixon <90346288+hannahmnixon@users.noreply.github.com>
2021-09-14 15:14:16 -0700

    added current tem, humidity & wind info

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity + "%";
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed) + " km/h";
 }
 let apiKey = "a6ff4b7a9b12bc3ba444e702f94356c3";
-let apiUrl = `api.openweathermap.org/data/2.5/weather?q=San Francisco&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=San Francisco&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
