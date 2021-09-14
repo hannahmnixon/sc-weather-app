03c09036b75914e5360c573498ae756c8fb07441
hannahmnixon <90346288+hannahmnixon@users.noreply.github.com>
2021-09-14 15:42:30 -0700

    updated icon functionality

--- src/index.js
+++ src/index.js
@@ -34,6 +34,12 @@ function displayTemperature(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed) + " km/h";
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 let apiKey = "a6ff4b7a9b12bc3ba444e702f94356c3";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=San Francisco&appid=${apiKey}&units=metric`;
