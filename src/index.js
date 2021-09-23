7ab102e78f07e11f3e469d1ef1842033b52b5d6f
hannahmnixon <90346288+hannahmnixon@users.noreply.github.com>
2021-09-22 17:22:47 -0700

    added forecast api

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
@@ -47,6 +47,12 @@ function displayForecast() {
   });
 }
 
+function getForecast(coordinates) {
+  let apiKey = "a6ff4b7a9b12bc3ba444e702f94356c3";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -68,6 +74,8 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
   celciusTemperature = Math.round(response.data.main.temp);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -107,4 +115,3 @@ celciusLink.addEventListener("click", displayCelciusTemperature);
 let celciusTemperature = null;
 
 search("San Francisco");
-displayForecast();
